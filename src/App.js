80955a89724dc677e8d247d36cfbad2bb6325655
François Lyons <lyons.fr55@gmail.com>
2022-08-08 09:04:38 +0200

    ajout du css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import './categories.styles.scss';
 const App= () => {
 
   const categories = [
@@ -32,10 +32,8 @@ const App= () => {
   return (
   <div className="categories-container">
     {categories.map((category)=>(
-
-    
     <div className="category-container">
-      {/* <img /> */}
+      <div className='background-image' />
       <div className="category-body-container">
         <h2>{category.title}</h2>
         <p>Shop Now</p>
