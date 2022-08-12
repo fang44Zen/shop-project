ac1e77d19740392c872b22218a41863eab805b4e
François Lyons <lyons.fr55@gmail.com>
2022-08-12 14:12:23 +0200

    ajout de la bar de navigation et de ces routes, ajout de la DB avec firebase en cour...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import {Route, Routes} from 'react-router-dom';
-
+import Navigation from './components/routes/navigation/navigation.component';
 import Home from './components/routes/home/home.component';
+
 
 const App= () => {
 
+
+  const Shop = () =>{
+    return(
+      <h1>I am in the shop page</h1>
+    )
+  }
+
   return (
-    <Routes>
-      <Route path='/home' element={<Home/>}/>
       
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home/>}/>
+        <Route path="/shop" element={<Shop />}/>
+      </Route>
     </Routes>
   );
   
