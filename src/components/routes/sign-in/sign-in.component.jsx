4aa6c3624e2f35988d8be3f204173a021521010a
François Lyons <lyons.fr55@gmail.com>
2022-08-13 20:47:49 +0200

    création du sign in avec firebase, j'ai rien compris lol

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,18 @@
+import { signInWithGooglePopup, creatUserDocumentFromAuth } from '../../../utils/firebase/firebase.utils'
+
 
 
 const SignIn = () =>{
+
+    const logGoogleUser = async () =>{
+        const {user} = await signInWithGooglePopup();
+        creatUserDocumentFromAuth(user);
+    }
+
     return(
         <div>
             <h1>Sign In page</h1>
+            <button onClick={logGoogleUser}>Sign in with Google</button>
         </div>
     )
 }
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,41 @@
+import { initializeApp } from 'firebase/app';
+import { 
+    getAuth, 
+    signInWithRedirect, 
+    signInWithPopup, 
+    GoogleAuthProvider 
+} from 'firebase/auth';
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAFTheaji61oa0_VEapro89B5oQuYCpCNc",
+    authDomain: "shop-tutorial-db-568d0.firebaseapp.com",
+    projectId: "shop-tutorial-db-568d0",
+    storageBucket: "shop-tutorial-db-568d0.appspot.com",
+    messagingSenderId: "698415583800",
+    appId: "1:698415583800:web:cca8e63dafa1cb5317cccd"
+  };
+  
+  // Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+  });
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup= () => signInWithPopup(auth, provider);
+  export const db = getFirestore();
+
+  export const creatUserDocumentFromAuth = async (userAuth) =>{
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+    const userSnapshot = await getDoc(userDocRef);
+  }
