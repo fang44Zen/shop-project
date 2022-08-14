a1be9bb5af6266c6af5d27b92b6597e18ab64c4a
François Lyons <lyons.fr55@gmail.com>
2022-08-14 18:18:09 +0200

    ajout d'une authentification sans google, rien n'est connecté à la db pour le moment. PS: on comprend un peux mieux mais c'est pas trop ca lol

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,21 @@
-import { signInWithGooglePopup, creatUserDocumentFromAuth } from '../../../utils/firebase/firebase.utils'
+import { signInWithGooglePopup, creatUserDocumentFromAuth, singInWithGoogleRedirect, auth } from '../../../utils/firebase/firebase.utils'
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import SignUp from '../../sing-up/sing-up.component';
 
 
 
 const SignIn = () =>{
+
+    useEffect(()=>{
+        getRedirectResult( async() =>{
+            const response = await getRedirectResult(auth);
+            if(response){
+                const userDocRef = await creatUserDocumentFromAuth(response.user)
+            }
+        });
+    }, []);
+
 
     const logGoogleUser = async () =>{
         const {user} = await signInWithGooglePopup();
@@ -13,6 +26,8 @@ const SignIn = () =>{
         <div>
             <h1>Sign In page</h1>
             <button onClick={logGoogleUser}>Sign in with Google</button>
+            <button onClick={singInWithGoogleRedirect}>Sign in with Google Redirect</button>
+            <SignUp />
         </div>
     )
 }
---
+++ src/components/sing-up/sing-up.component.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password:'',
+    confirmPassword: '',
+}
+
+const SignUp = () =>{
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const {displayName, email, password, confirmPassword} = formFields;
+
+    const handleChange = (event) =>{
+        const {name, value} = event.target;
+
+        setFormFields({...formFields, [name]: value});
+    };
+
+    return(
+        <div>
+            <h1>Sign up with you email and password</h1>
+            <form onSubmit={()=>{}}>
+                <label>Dispalay name</label>
+                <input required type='text' onChange={handleChange} name='displayName' value={displayName}/>
+
+                <label>Email</label>    
+                <input required type="email" onChange={handleChange}  name='email' value={email}/>
+
+                <label>Password</label>
+                <input required type="password" onChange={handleChange}  name='password'  value={password}/>
+
+                <label>Confirm Password</label>
+                <input required type='password' onChange={handleChange}  name='confirm password' value={confirmPassword}/>
+                <button type='submit'>Sign up</button>
+            </form>
+        </div>
+       
+    );
+};
+
+export default SignUp;
