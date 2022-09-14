8ae1b0ea112b202a2f71ea000ef55d39c67ddc09
François Lyons <lyons.fr55@gmail.com>
2022-09-14 20:03:31 +0200

    context fonctionnel dans le component 'shop'

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,10 +1,11 @@
-import SHOP_DATA from '../../../shop-data.json';
-
+import { useContext } from "react";
+import {ProdctContext} from '../../../contexts/products.context';
 
 const Shop = () =>{
+    const {products} = useContext(ProdctContext);
     return(
         <div>
-            {SHOP_DATA.map(({id, name}) =>(
+            {products.map(({id, name}) =>(
                 <div key={id}>
                     <h1>{name}</h1>
                 </div>
