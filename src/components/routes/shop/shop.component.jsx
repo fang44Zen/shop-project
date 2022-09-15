181160be62afe11f0bbe7d281e2e41570f34a4ad
François Lyons <lyons.fr55@gmail.com>
2022-09-15 16:32:15 +0200

    card-shop oppérationnel (sauf boutton)

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,14 +1,14 @@
 import { useContext } from "react";
 import {ProdctContext} from '../../../contexts/products.context';
+import ProductCard from "../../products-card/products-card.component";
+import './shop.styles.scss';
 
 const Shop = () =>{
     const {products} = useContext(ProdctContext);
     return(
-        <div>
-            {products.map(({id, name}) =>(
-                <div key={id}>
-                    <h1>{name}</h1>
-                </div>
+        <div className="products-container">
+            {products.map((product) =>(
+                <ProductCard key={product.id} product={product}/>
             ))}
         </div>
     )
