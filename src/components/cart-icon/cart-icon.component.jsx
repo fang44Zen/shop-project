89fb102bf721f82746c2c49213671dc224669f30
François Lyons <lyons.fr55@gmail.com>
2022-10-13 22:43:13 +0200

    ajout/correction du bug home page

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,7 +7,7 @@ import './cart-icon.styles.scss';
 const CartIcon = () =>{
     const {isCartOpen, setIsCartOpen} = useContext(CartContext);
 
-    const toggleIsCartOpen = () =>setIsCartOpen(!isCartOpen);
+    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
     return(
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon  className='shopping-icon'/>
